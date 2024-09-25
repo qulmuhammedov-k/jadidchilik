1727c5b34860128b495cf040d0592f6b1b3f6e92
Komil Qulmuhammedov <qulmuhammedov_k@mail.ru>
2024-09-25 18:44:55 +0500

    iOS Next update

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-// Video elementini tanlash
 let videoElement = document.getElementById('countdown-video');
-const headerDiv = document.querySelector('.header')
-const containerDiv = document.querySelector('.container')
-const footerDiv = document.querySelector('.footer')
+const headerDiv = document.querySelector('.header');
+const containerDiv = document.querySelector('.container');
+const footerDiv = document.querySelector('.footer');
 
-function bodyShow(e){
-    e.classList.add('show')
-    e.classList.remove('hide')
-}
 // Yuklanish sahifasi va asosiy kontentni tanlash
 let loadingScreen = document.getElementById('loading-screen');
 let mainContent = document.getElementById('main-content');
+
+// Foydalanuvchi harakati orqali videoni boshlatish
+document.body.addEventListener('click', function () {
+    // Agar video boshlanmagan bo'lsa, uni boshlatish
+    if (videoElement.paused) {
+        videoElement.play();
+    }
+});
 
 // Video tugagandan so'ng asosiy sahifani ko'rsatish
 videoElement.onended = function() {
     loadingScreen.style.display = 'none';
     mainContent.style.display = 'block';
-    bodyShow(headerDiv)
-    bodyShow(containerDiv)
-    bodyShow(footerDiv)
+    bodyShow(headerDiv);
+    bodyShow(containerDiv);
+    bodyShow(footerDiv);
 };
+
+// Classlarni to'g'rilab beradigan funksiya
+function bodyShow(e) {
+    e.classList.add('show');
+    e.classList.remove('hide');
+}
